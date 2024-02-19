64642d2cc14103e524799536b3b4c6785680d6f4
Joel Lichtenberg <j.lichtenberg@blauwolk.com>
2024-02-19 16:54:21 +0100

    added blogs to models

---
+++ app/Models/Blog.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'blogs';
+
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'title',
+        'image',
+        'body',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
